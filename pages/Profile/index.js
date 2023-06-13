faf12ca1d67952ad8856485cd3363041063ca133
Lucas Xavier <lucassxxavier@gmail.com>
2023-06-13 16:35:01 -0300

    feat: finishing profile page

--- pages/Profile/index.js
+++ pages/Profile/index.js
@@ -12,3 +12,36 @@ function appendHtmlString(parentElement, htmlString) {
 const topContainer = document.getElementById('top-container');
 appendHtmlString(topContainer, Drawer());
 appendHtmlString(topContainer, Header());
+
+const tabButtons = document.querySelectorAll('.tab-button');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const tabId = button.getAttribute('data-tab');
+    activateTab(tabId);
+  });
+});
+
+const tabButton = document.querySelector(`[data-tab="tab1"]`);
+const tabContent = document.getElementById('tab1');
+
+tabButton.classList.add('active');
+tabContent.classList.add('active');
+
+function activateTab(tabId) {
+  console.log(tabId)
+  tabButtons.forEach(button => {
+    button.classList.remove('active');
+  });
+
+  tabContents.forEach(content => {
+    content.classList.remove('active');
+  });
+
+  const tabButton = document.querySelector(`[data-tab="${tabId}"]`);
+  const tabContent = document.getElementById(tabId);
+
+  tabButton.classList.add('active');
+  tabContent.classList.add('active');
+}
